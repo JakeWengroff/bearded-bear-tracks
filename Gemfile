11694e9cd3db4b32523754f141f315255e7e544a
Jake Wengroff <j@jxb1.com>
2015-06-01 21:58:57 -0400

    Installed Mailgun, Topic and Bookmark models and controllers, and RSpec.

--- Gemfile
+++ Gemfile
@@ -47,7 +47,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug'
+  # gem 'byebug'
+  gem 'rspec-rails', '~>3.0'
 
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
---
+++ app/controllers/incoming_controller.rb
@@ -0,0 +1,27 @@
+class IncomingController < ApplicationController
+
+  # http://stackoverflow.com/questions/1177863/how-do-i-ignore-the-authenticity-token-for-specific-actions-in-rails
+  skip_before_action :verify_authenticity_token, only: [:create]
+
+  def create
+    @user = User.find_by(email: params[:sender])
+    @topic = Topic.find_by(title: params[:subject])
+
+    @url = params["body-plain"]
+
+    if @user.nil?
+      @user = User.new(email: params[:sender], password: "temp0rary_passw0rd")
+      @user.skip_confirmation!
+      @user.save!
+    end
+
+    if @topic.nil?
+      @topic = @user.topics.create(title: params[:subject])
+    end
+
+    @bookmark = @user.bookmarks.create(url: @url, topic_id: @topic.id)
+
+    head 200
+  end
+end
+
