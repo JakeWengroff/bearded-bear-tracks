404b881f22812b2a927cafd896466dfb9eeb2482
Brittany Martin <brittany.jill.martin@gmail.com>
2015-06-14 09:55:11 -0400

    Fixed the Model tests

--- spec/factories/bookmark.rb
+++ spec/factories/bookmark.rb
@@ -3,5 +3,6 @@ require 'factory_girl'
 FactoryGirl.define do
   factory :bookmark do
     url   Faker::Internet.url
+    topic
   end
 end
--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -1,7 +1,15 @@
 require 'spec_helper'
 
-RSpec.describe Bookmark, type: :model do
-  it { should validate_presence_of :topic }
-  it { should validate_presence_of :url }
-end
+RSpec.describe Topic, type: :model do
+  it "should have valid factory" do
+    FactoryGirl.build(:bookmark).should be_valid
+  end
 
+  it "should require a topic" do
+      FactoryGirl.build(:bookmark, :topic_id => "").should_not be_valid
+  end
+
+  it "should require a url" do
+      FactoryGirl.build(:bookmark, :url => "").should_not be_valid
+  end
+end
