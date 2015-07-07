bb9757d3c56a34d3066054d5e01e4ccd15fc2d6a
Brittany Martin <brittany.jill.martin@gmail.com>
2015-07-07 08:08:24 -0400

    Working view of Userbookmarks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @created_bookmarks = current_user.created_bookmarks
+    @created_bookmarks = current_user.bookmarks
     @liked_bookmarks = current_user.liked_bookmarks
   end
 end
