4b7cf9cc714c7eaac1a67653d8afab3e04d5fa88
Jake Wengroff <j@jxb1.com>
2015-07-17 20:40:02 -0400

    Updated Production.rb.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'vast-shelf-9167.herokuapp.com' }
+
+  config.force_ssl = true
+
+
 end
